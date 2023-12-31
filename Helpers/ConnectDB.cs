e84743c4f76cca44f13d66cc4a0ef17f74493c9d
abdelrahmman97 <mabdelrahmman22@gmail.com>
2023-12-31 15:03:04 +0200

    updated ConnectDB

--- Helpers/ConnectDB.cs
+++ Helpers/ConnectDB.cs
@@ -4,59 +4,96 @@ using System.Data.SqlClient;
 
 namespace WebForms.SocialNetwork.Helpers
 {
-	public class ConnectDB
-	{
-		private readonly string _connectionString = "";
-		private SqlConnection _connection = new SqlConnection();
-		private SqlCommand _command = new SqlCommand();
-		private DataTable dt = new DataTable();
+    public class ConnectDB
+    {
+        private readonly string _connectionString = "";
+        private SqlConnection _connection = new SqlConnection();
+        private SqlCommand _command = new SqlCommand();
+        private DataTable dt = new DataTable();
+        private SqlCommand cmd = new SqlCommand();
+        private SqlDataReader reader = null;
 
-		public ConnectDB( string connection )
-		{
-			_connectionString = ConfigurationManager.ConnectionStrings[ connection ].ConnectionString;
-		}
 
-		public void StartConnection()
-		{
-			_connection.Close();
-			_connection.ConnectionString = _connectionString;
-			_connection.Open();
-			_command.Connection = _connection;
-			_command.CommandTimeout = 999999;
-		}
+        public ConnectDB( string connection )
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings[ connection ].ConnectionString;
+        }
 
-		public DataTable ExecuteQuery( string query )
-		{
-			try
-			{
-				StartConnection();
-				_command.CommandText = query;
-				using ( SqlDataAdapter adapter = new SqlDataAdapter( _command ) )
-				{
-					adapter.Fill( dt );
-				}
-			}
-			finally
-			{
-				_connection.Close();
-			}
-			return dt;
-		}
+        public void StartConnection()
+        {
+            _connection.Close();
+            _connection.ConnectionString = _connectionString;
+            _connection.Open();
+            _command.Connection = _connection;
+            _command.CommandTimeout = 999999;
+        }
 
-		public int ExecuteNonQuery( string query )
-		{
-			int rowsAffected = 0;
-			try
-			{
-				StartConnection();
-				_command.CommandText = query;
-				rowsAffected = _command.ExecuteNonQuery();
-			}
-			finally
-			{
-				_connection.Close();
-			}
-			return rowsAffected;
-		}
-	}
+        public DataTable ExecuteQuery( string query )
+        {
+            try
+            {
+                StartConnection();
+                _command.CommandText = query;
+                using ( SqlDataAdapter adapter = new SqlDataAdapter( _command ) )
+                {
+                    adapter.Fill( dt );
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return dt;
+        }
+
+        public int ExecuteNonQuery( string query )
+        {
+            int rowsAffected = 0;
+            try
+            {
+                StartConnection();
+                _command.CommandText = query;
+                rowsAffected = _command.ExecuteNonQuery();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return rowsAffected;
+        }
+
+        public void AddWithValue( string parameter, string value )
+        {
+            cmd.Parameters.AddWithValue( parameter, value );
+        }
+
+        public SqlDataReader ExecuteQueryWithValues( string query )
+        {
+            try
+            {
+                StartConnection();
+                reader = cmd.ExecuteReader();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return reader;
+        }
+
+        public int ExecuteNonQueryWithValues()
+        {
+            try
+            {
+                StartConnection();
+                return cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+
+    }
 }
